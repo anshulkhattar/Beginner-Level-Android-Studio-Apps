e2b12e501ae836047b9e8b156fb018aa2d164748
gvking <vikram.pasupathy@gmail.com>
2018-10-26 14:38:25 -0700

    Update ExampleInstrumentedTest.java

--- TheQuizApp/app/src/androidTest/java/satyajitnets/thequizapp/ExampleInstrumentedTest.java
+++ TheQuizApp/app/src/androidTest/java/satyajitnets/thequizapp/ExampleInstrumentedTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
+import java.util.Scanner;
 
 /**
  * Instrumented test, which will execute on an Android device.
