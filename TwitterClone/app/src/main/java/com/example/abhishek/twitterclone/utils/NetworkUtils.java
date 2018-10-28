2731fdfe9a445230bd1936efc73824d2b5d556a7
AbhishekChd <acechd97@gmail.com>
2018-10-28 06:37:40 +0530

    Added Twitter UI Clone

---
+++ TwitterClone/app/src/main/java/com/example/abhishek/twitterclone/utils/NetworkUtils.java
@@ -0,0 +1,21 @@
+package com.example.abhishek.twitterclone.utils;
+
+public class NetworkUtils {
+    private final String LOG_TAG = getClass().getSimpleName();
+
+    /*
+    Request Format
+    curl –request GET
+     –url https://stream.twitter.com/1.1/statuses/sample.json
+     –header ‘authorization: OAuth oauth_consumer_key=”CONSUMER_KEY”,
+      oauth_nonce=”CONSUMER_SECRET”,
+      oauth_signature=”GENERATED_SIGNATURE”,
+      oauth_signature_method=”HMAC-SHA1”,
+      oauth_timestamp=”GENERATED_TIMESTAMP”,
+      oauth_token=”ACCESS_TOKEN”,
+      oauth_version=”1.0”’`
+     */
+
+
+
+}
