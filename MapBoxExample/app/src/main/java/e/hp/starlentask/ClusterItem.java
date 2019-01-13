3edb301e14b3fdb09b65df36fa96ec0890b4fe17
Anshul <anshul070300@gmail.com>
2019-01-13 10:14:42 +0530

    MapBoxExample

---
+++ MapBoxExample/app/src/main/java/e/hp/starlentask/ClusterItem.java
@@ -0,0 +1,23 @@
+package e.hp.starlentask;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class ClusterItem {
+    LatLng getPosition() {
+        return null;
+    }
+
+    /**
+     * The title of this marker.
+     */
+    String getTitle() {
+        return null;
+    }
+
+    /**
+     * The description of this marker.
+     */
+    String getSnippet() {
+        return null;
+    }
+}
