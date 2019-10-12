308efbcf25548f9ef1380c054ef268138bd14d74
josancamon19 <joan.santiago.cabezas@gmail.com>
2019-10-11 20:16:49 -0500

    BarcodeReader project migrated to androidx

--- BarcodeReader/app/src/main/java/com/tc2r/barcodereader/MainActivity.java
+++ BarcodeReader/app/src/main/java/com/tc2r/barcodereader/MainActivity.java
@@ -2,12 +2,11 @@ package com.tc2r.barcodereader;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-import java.util.Scanner;
 
 import com.google.zxing.intergration.android.IntentIntegrator;
 import com.google.zxing.intergration.android.IntentResult;
