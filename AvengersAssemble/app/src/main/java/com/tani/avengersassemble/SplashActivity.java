67f93f39db9b2f4d15805d6fe72e6aa95e4e07ac
Priyanka Kumari <priyankalily0915@gmail.com>
2020-10-29 22:40:05 +0530

    Avengers Assemble

---
+++ AvengersAssemble/app/src/main/java/com/tani/avengersassemble/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.tani.avengersassemble;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(SplashActivity.this,
+                        MainActivity.class);
+                //Intent is used to switch from one activity to another.
+
+                startActivity(i);
+                //invoke the SecondActivity.
+
+                finish();
+                //the current activity will get finished.
+            }
+        }, 3000);
+
+    }
+}
