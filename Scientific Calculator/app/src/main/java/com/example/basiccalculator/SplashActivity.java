64749487a67a382ab809d07af8e54532120e32b5
Priyanka Kumari <priyankalily0915@gmail.com>
2020-10-29 16:51:47 +0530

    scientific_calc

---
+++ Scientific Calculator/app/src/main/java/com/example/basiccalculator/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.basiccalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, 3000);
+
+    }
+
+
+}
