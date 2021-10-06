71dfff392d45db4bd2acf5cba6407c8ffc3d010b
Aman Kumar <amankumar33152@gmail.com>
2021-10-06 14:00:51 +0530

    Added a basic app for beginners

---
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice() {
+        val dice = Dice()
+        val diceNumber = dice.roll()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        diceImage.contentDescription = diceNumber.toString()
+
+        val drawableResource = when (diceNumber){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+    }
+}
+class Dice(){
+    fun roll(): Int { return (1..6).random() }
+}
